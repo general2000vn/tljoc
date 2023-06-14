55c064735467bb325af0488c594ba27f1e2794b5
Tran Hoang Anh <general2001vn@yahoo.com>
2023-06-14 16:00:03 +0700

    edit copy right 2

--- templates/element/sash/footer.php
+++ templates/element/sash/footer.php
@@ -3,7 +3,7 @@
             <div class="container">
                 <div class="row align-items-center flex-row-reverse">
                     <div class="col-md-12 col-sm-12 text-center">
-                        Copyright © <span id="year"></span>. Developed by Trần Hoàng Anh. All rights reserved.
+                        Copyright © <span id="year"></span>. Developed by Luton. All rights reserved.
                     </div>
                 </div>
             </div>
